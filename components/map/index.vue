a7785fb15856ee3493e4a42d1dfa979318d1286c
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-08-28 13:46:09 -0300

    fallback para client only

--- components/map/index.vue
+++ components/map/index.vue
@@ -1,25 +1,30 @@
 <template>
     <div id="map">
-      <LMap 
-        :zoom="5" 
-        :center="[-15.793889, -47.882778]"
-        :use-global-leaflet="true"
-      >
-        <LTileLayer
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          layerType="base"
-          name="OpenStreetMap"
-        />
-      </LMap>
+      <ClientOnly fallback-tag="span" fallback="Loading map...">
+        <LMap 
+          :zoom="5" 
+          :center="[-15.793889, -47.882778]"
+          :use-global-leaflet="false"
+        >
+          <LTileLayer
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            layer-type="base"
+            name="OpenStreetMap"
+          />
+          <Marks />
+        </LMap>
+      </ClientOnly>
     </div>
 </template>
 
 <script setup lang="ts">
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+import Marks from "@/components/map/marks/index.vue";
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 #map {
-    @apply w-full h-[80vh];
+    @apply w-full h-full;
 }
 </style>
