afc95ecee207999f6b79a4cfaea0d519dea797c1
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-08-30 18:06:51 -0300

    header para facilitar transição no site

---
+++ components/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <NuxtLink class="logo" to="/">
+            <img
+                src="/images/aiko.png"
+                alt="Logo Aiko"
+                width="50"
+                height="24"
+            >
+        </NuxtLink>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+    .header {
+        @apply flex items-center justify-center;
+        @apply w-full;
+        @apply px-8 py-3;
+        @apply bg-white;
+        @apply rounded-t-lg;
+    }
+</style>
