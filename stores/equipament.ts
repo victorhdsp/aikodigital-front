dff05ffe427481f84e1f649a5d31bb8d05c27c53
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-08-31 15:38:18 -0300

    filtros stores pages

--- stores/equipament.ts
+++ stores/equipament.ts
@@ -1,14 +1,59 @@
 import { defineStore } from "pinia";
 import type { EquipamentComplete } from "~/assets/types/equipament";
+import type { OrganizedStateHistory } from "@/assets/types/equipament";
+import { organizeStateHistory } from "~/components/aside/equipament/history/useHistory";
 import { getEquipament } from "~/lib/api";
 
+function treatment (str: string) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+}
+
 export const useEquipamentStore = defineStore("equipament", () => {
+    const baseStateHistory = ref<OrganizedStateHistory[]>([]);
+    const stateHistory = ref<OrganizedStateHistory[]>([]);
     const item = ref<EquipamentComplete>();
+
+    const filters = ref({
+        search: "",
+        status: ""
+    });
 
     async function fetchById (id:string): Promise<EquipamentComplete> {
         item.value = await getEquipament(id);
+        baseStateHistory.value = organizeStateHistory(
+            item.value.stateHistory, 
+            item.value.positionHistory
+        );
+        stateHistory.value = baseStateHistory.value;
         return item.value;
     }
+
+    function filterBySearch (search: string) {
+        filters.value.search = treatment(search);
+        filterList();
+    }
+    function filterByStatus (status: string) {
+        filters.value.status = treatment(status);
+        filterList();
+    }
+
+    function filterList () {
+        const { search, status } = filters.value;
+        const filteredStatus = baseStateHistory.value?.map((item) => {
+            const states = item.states.filter((state) => {
+                return treatment(state.name).includes(status);
+            });
+            return { ...item, states };
+        }).filter((item) => item.states.length > 0);
+        
+        const filteredSearch = filteredStatus?.filter((item) => {
+            if (treatment(item.date.toLocaleString()).includes(search)) return true;
+            return false;
+        });
+        
+        stateHistory.value = filteredSearch;
+    }
     
-    return { item, fetchById };
+    
+    return { item, stateHistory, fetchById, filterBySearch, filterByStatus };
 });
--- stores/equipaments.ts
+++ stores/equipaments.ts
@@ -2,47 +2,48 @@ import { defineStore } from "pinia";
 import type { EquipamentSimple } from "~/assets/types/equipament";
 import { getEquipamentList } from "~/lib/api";
 
+function treatment (str: string) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+}
+
 export const useEquipamentsStore = defineStore("equipaments", () => {
     const baseList = ref<EquipamentSimple[]>([]);
-    const filteredList = ref<EquipamentSimple[]>([]);
     const list = ref<EquipamentSimple[]>([]);
+
+    const filters = ref({
+        search: "",
+        status: ""
+    });
 
     async function fetchList (): Promise<EquipamentSimple[]> {
         baseList.value = await getEquipamentList();
-        list.value = baseList.value
-        filteredList.value = baseList.value
+        list.value = baseList.value;
         return list.value;
     }
 
-    function filterBySearch (search: string): void {
-        if (!search) {
-            list.value = filteredList.value;
-            return;
-        }
-        
-        list.value = filteredList.value.filter((equipament) => {
-            const content = [
-                equipament.name,
-                equipament.vehicle.name,
-                equipament.state.name
-            ].join(" ");
-            
-            const treatment = (content: string) => content.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-
-            return treatment(content).includes(treatment(search));
-        });
+    function filterBySearch (search: string) {
+        filters.value.search = treatment(search);
+        filterList();
+    }
+    function filterByStatus (status: string) {
+        filters.value.status = treatment(status);
+        filterList();
     }
 
-    function filterByTag (tag: string): void {
-       if (!tag) {
-           list.value = baseList.value;
-           return;
-       }
+    function filterList () {
+        const { search, status } = filters.value;
+        const filteredStatus = baseList.value.filter((equipament) => {
+            return status ? treatment(equipament.state.name) === status : true;
+        });
+        const filteredSearch = filteredStatus.filter((equipament) => {
+            if (treatment(equipament.name).includes(search)) return true;
+            if (treatment(equipament.vehicle.name).includes(search)) return true;
+            if (treatment(equipament.lastUpdate).includes(search)) return true;
+            return false;
+        });
 
-         list.value = baseList.value.filter((equipament) => {
-              return equipament.state.name === tag;
-         });
+        list.value = filteredSearch;
     }
     
-    return { list, fetchList, filterBySearch, filterByTag };
+    return { list, fetchList, filterBySearch, filterByStatus};
 });
