2bacdd6a54c403fdbbfd67c6fe028fec6ebcac77
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-08-31 15:34:08 -0300

    mudanças no local dos tipos

--- assets/types/equipament.d.ts
+++ assets/types/equipament.d.ts
@@ -1,3 +1,17 @@
+export interface PositionHistory {
+    date: string;
+    lat: number;
+    lon: number;
+}
+
+export interface StateHistory {
+    date: string;
+    state: {
+        name: string;
+        color: string;
+    };
+}
+
 export interface EquipamentComplete {
     id: string;
     name: string;
@@ -12,18 +26,8 @@ export interface EquipamentComplete {
             value: number;
         }[];
     };
-    positionHistory: {
-        date: string;
-        lat: number;
-        lon: number;
-    }[];
-    stateHistory: {
-        date: string;
-        state: {
-            name: string;
-            color: string;
-        };
-    }[];
+    positionHistory: PositionHistory[];
+    stateHistory: StateHistory[];
 }
 
 export interface EquipamentSimple {
@@ -40,4 +44,17 @@ export interface EquipamentSimple {
         name: string;
         color: string;
     };
+}
+
+export interface OrganizedStateHistory {
+    date: Date;
+    position: {
+        lat: number;
+        lon: number;
+    },
+    states: {
+        time: string;
+        color: string;
+        name: string;
+    }[];
 }
